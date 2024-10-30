75e8f384d2b118cd4b6042c0bc8ec6ec281e799a
lukaakhaladze <healerluka@gmail.com>
2024-10-31 00:16:53 +0400

    Chore: refactor code

--- src/main.py
+++ src/main.py
@@ -2,9 +2,10 @@ import dotenv
 import os
 from speaak_with_gpt import SpeakWithGPT
 from web_scraper import WebScraper
+from selector import Selector
 
 #todo this should be passed from client side
-GPT_analyzer_as = ("You are DevOps engineer with knowledge of python and "
+GPT_analyzer_as = ("You are Data analytics with knowledge of python and Sql"
                    "looking for job title return only answer 1 or 0 if it is compatible title for you knowledge")
 
 dotenv.load_dotenv("../.env")
@@ -13,11 +14,10 @@ OPENAI_API_KEY : str = os.getenv("OPENAI_API_KEY")
 SITES_URL : list[str] = [os.getenv("SITES_URL")] #for testing purposes after development it should take list of urls
 
 web_scraper = WebScraper(SITES_URL)
-applications = web_scraper.turn_into_list()
-
+applications = web_scraper.turn_titles_into_list()
 
 gpt = SpeakWithGPT(system_role=GPT_analyzer_as)
-#todo this code is checking titles of applications and using chatgpt api evaluates whether it is useful or not
-#move this to class in future when you create logic class
-for application in applications:
-    application["useful"] = gpt.prompt(application["title"]).choices[0].message.content
+useful_applications = Selector.select_titles(gpt_model=gpt,applications=applications)
+
+
+

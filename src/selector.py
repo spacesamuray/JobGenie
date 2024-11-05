92c45cbacde79a46443ed05a8aa9bcca09d34cf2
lukaakhaladze <healerluka@gmail.com>
2024-11-05 21:52:57 +0400

    Feat: added functionality to analyze_data method which now can analyze data so that it could scrap from different redirects

--- src/selector.py
+++ src/selector.py
@@ -1,4 +1,7 @@
+from selenium.webdriver.support.expected_conditions import title_is
+
 from speaak_with_gpt import SpeakWithGPT
+from web_scraper import WebScraper
 
 
 class Selector:
@@ -23,11 +26,25 @@ class Selector:
     #methods: turn_applications_into_list(),turn_titles_into_list()
     @staticmethod
     def analyze_data(gpt_model : SpeakWithGPT,applications : list[dict[str,str]]):
+        """
+        This method is responsible for analyzing data which is job description and which is not
+        :param gpt_model:
+        :param applications:
+        :return:
+        """
         evaluated_data : list[dict[str,str]] = []
         for application in applications:
             response = gpt_model.prompt(application).choices[0].message.content
             if response != '1':
-                pass
+                bs_html = WebScraper.scrap_web([response])[0]
+                item = {
+                    "title": application["title"],
+                    "data": bs_html,
+                }
+                evaluated_data.append(item)
+            else: evaluated_data.append(application)
+        return evaluated_data
+
 
     @staticmethod
     def select_application(gpt_model : SpeakWithGPT,applications : list[dict[str,str]]):
